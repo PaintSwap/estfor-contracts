5eb9f8d8effd81f9b9b68aed099e5dcdecb8d238
0xSamWitch <julian.assange12@outlook.com>
2023-01-11 15:06:33 +0000

    Update deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,35 +1,83 @@
 import {ethers} from "hardhat";
-import {createPlayer, Items} from "./utils";
+import {Attribute, createPlayer, EquipPosition, getActionId, Item, Skill} from "./utils";
 
 async function main() {
   const [owner, alice] = await ethers.getSigners();
 
   const MockBrushToken = await ethers.getContractFactory("MockBrushToken");
-  const mockBrushToken = await MockBrushToken.deploy();
+  const brush = await MockBrushToken.deploy();
 
-  // TODO: Shop
+//  if (network)
+
+  const MockOracleClient = await ethers.getContractFactory("MockOracleClient");
+  const mockOracleClient = await MockOracleClient.deploy();
+
+  // Create the world
+  const subscriptionId = 2;
+  const World = await ethers.getContractFactory("World");
+  const world = await World.deploy(mockOracleClient.address, subscriptionId);
+  await world.deployed();
+  console.log(`World deployed at ${world.address.toLowerCase()}`);
 
   // Create NFT contract which contains all items & players
-  const NFT = await ethers.getContractFactory("TestPaintScapeNFT");
-  const nft = await NFT.deploy(mockBrushToken.address);
-  await nft.deployed();
-  console.log(`NFT deployed at ${nft.address.toLowerCase()}`);
+  const Users = await ethers.getContractFactory("Users");
+  const users = await Users.deploy();
+  await users.deployed();
+
+  // Create NFT contract which contains all items & players
+  const ItemNFT = await ethers.getContractFactory("TestItemNFT");
+  const itemNFT = await ItemNFT.deploy(brush.address, world.address, users.address);
+  await itemNFT.deployed();
+  console.log(`Item NFT deployed at ${itemNFT.address.toLowerCase()}`);
+
+  // Create NFT contract which contains all the players
+  const PlayerNFT = await ethers.getContractFactory("PlayerNFT");
+  const playerNFT = await PlayerNFT.deploy(brush.address, itemNFT.address, world.address, users.address);
+  await playerNFT.deployed();
+  console.log(`Player NFT deployed at ${playerNFT.address.toLowerCase()}`);
+
+  let tx = await itemNFT.setPlayerNFT(playerNFT.address);
+  await tx.wait();
+  tx = await users.setNFTs(playerNFT.address, itemNFT.address);
+  await tx.wait();
+
+  const avatarId = 1;
+  const avatarInfo = {name: "Name goes here", description: "Hi I'm a description", imageURI: "1234.png"};
+  tx = await playerNFT.addAvatar(avatarId, avatarInfo);
+  await tx.wait();
 
   // Create player
-  const player = await createPlayer(nft, alice);
-  console.log(`Player deployed at ${player.address.toLowerCase()}`);
+  const playerId = await createPlayer(playerNFT, avatarId, alice, ethers.utils.formatBytes32String("0xSamWitch"));
 
-  const maxTime = await player.MAX_TIME();
-  const maxWeight = await player.MAX_WEIGHT_PER_SLOT();
+  // === Test stuff ===
+  await itemNFT.addItem(
+    Item.BRUSH,
+    {attribute: Attribute.ATTACK, equipPosition: EquipPosition.AUX, exists: true, canEquip: true, bonus: 0},
+    "someIPFSURI.png"
+  );
+  tx = await itemNFT.testMint(alice.address, Item.BRUSH, 1);
+  await tx.wait();
+  tx = await playerNFT.connect(alice).equip(playerId, Item.BRUSH);
+  await tx.wait();
 
-  // Test stuff
-  await player.connect(alice).paint();
+  tx = await world.addAction({
+    skill: Skill.PAINT,
+    baseXPPerHour: 10,
+    minSkillPoints: 0,
+    isDynamic: false,
+    itemPosition: EquipPosition.AUX,
+    itemTokenIdRangeMin: Item.BRUSH,
+    itemTokenIdRangeMax: Item.WAND,
+  });
+  const actionId = getActionId(tx);
+  tx = await world.setAvailable(actionId, true);
+  await tx.wait();
+
+  tx = await playerNFT.connect(alice).startAction(Skill.PAINT, 100, playerId, false);
+  await tx.wait();
   await ethers.provider.send("evm_increaseTime", [1]);
-  await player.connect(alice).consumeLastSkill();
-  await player.connect(alice).paint();
-
-  await nft.testMint(alice.address, Item.SHIELD, 1);
-  await player.connect(alice).addToInventory(Item.SHIELD, 1);
+  tx = await playerNFT.connect(alice).consumeSkills(playerId);
+  await tx.wait();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
