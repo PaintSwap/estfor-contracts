7d45163a8b86651574acd4d155e7baf628ac1408
Justin Silver <jsilver@doublesharp.com>
2023-03-11 12:02:36 -0800

    import IERC20

--- contracts/interfaces/IBrushToken.sol
+++ contracts/interfaces/IBrushToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IBrushToken is IERC20 {
   function burn(uint256 _amount) external;
