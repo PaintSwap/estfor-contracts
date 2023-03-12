9dba95ebb7e9563444a99a08a7fbcf630c8ee69e
Justin Silver <jsilver@doublesharp.com>
2023-03-12 14:09:58 -0700

    store path as immutable

--- contracts/RoyaltyReceiver.sol
+++ contracts/RoyaltyReceiver.sol
@@ -19,14 +19,18 @@ contract RoyaltyReceiver is Ownable {
   Router public immutable router;
   address public immutable pool;
   IBrushToken public immutable brush;
-  address[] public buyPath;
+  address private immutable buyPath1;
+  address private immutable buyPath2;
+
   uint public constant DEADLINE_DURATION = 10 minutes; // Doesn't matter
 
-  constructor(Router _router, address _pool, IBrushToken _brush, address[] memory _buyPath) {
+  constructor(Router _router, address _pool, IBrushToken _brush, address[2] memory _buyPath) {
     pool = _pool;
     router = _router;
     brush = _brush;
-    buyPath = _buyPath;
+    // store the path in the bytecode
+    buyPath1 = _buyPath[0];
+    buyPath2 = _buyPath[1];
     if (address(_router) == address(0)) {
       revert AddressZero();
     }
@@ -38,10 +42,16 @@ contract RoyaltyReceiver is Ownable {
     }
   }
 
+  function buyPath() public view returns (address[] memory _buyPath) {
+    _buyPath = new address[](2);
+    _buyPath[0] = buyPath1;
+    _buyPath[1] = buyPath2;
+  }
+
   receive() external payable {
     uint deadline = block.timestamp + DEADLINE_DURATION;
     // Buy brush and send it to the pool
-    uint[] memory amounts = router.swapExactETHForTokens{value: msg.value}(0, buyPath, address(this), deadline);
+    uint[] memory amounts = router.swapExactETHForTokens{value: msg.value}(0, buyPath(), address(this), deadline);
     brush.transfer(pool, amounts[amounts.length - 1]);
   }
 }
