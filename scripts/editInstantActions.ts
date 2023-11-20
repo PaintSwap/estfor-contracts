f823f2e1d2d850c7f619d5b2a0eeb6b692582765
0xSamWitch <julian.assange12@outlook.com>
2023-11-20 13:27:40 +0000

    Update some instant actions

--- scripts/editInstantActions.ts
+++ scripts/editInstantActions.ts
@@ -2,20 +2,24 @@ import {ethers} from "hardhat";
 import {INSTANT_ACTIONS_ADDRESS} from "./contractAddresses";
 import {EstforConstants} from "@paintswap/estfor-definitions";
 import {allInstantActions} from "./data/instantActions";
-import {InstantActionInput} from "@paintswap/estfor-definitions/types";
 
 async function main() {
   const [owner] = await ethers.getSigners();
   console.log(`Edit instant actions using account: ${owner.address} on chain id ${await owner.getChainId()}`);
 
   const instantActions = await ethers.getContractAt("InstantActions", INSTANT_ACTIONS_ADDRESS);
-  const action = allInstantActions.find((action) => {
-    if (action) {
-      return action.actionId == EstforConstants.INSTANT_ACTION_FORGING_IRON_ARMOR;
-    }
-  }) as InstantActionInput;
+  const actions = allInstantActions.filter(
+    (action) =>
+      action.actionId == EstforConstants.INSTANT_ACTION_FORGING_NATUOW_LEATHER ||
+      action.actionId == EstforConstants.INSTANT_ACTION_FORGING_ROPE ||
+      action.actionId == EstforConstants.INSTANT_ACTION_FORGING_ACORN_PATCH
+  );
 
-  await instantActions.editActions([action]);
+  if (actions.length !== 3) {
+    console.log("Cannot find all items");
+  } else {
+    await instantActions.editActions(actions);
+  }
 }
 
 main().catch((error) => {
