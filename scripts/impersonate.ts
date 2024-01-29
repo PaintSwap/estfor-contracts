a5ff48c20e347d131dd050ae88f026c0e26b9ec5
0xSamWitch <julian.assange12@outlook.com>
2024-01-29 18:46:35 +0000

    Add clans, lockedBankVault & territories to impersonation script

--- scripts/impersonate.ts
+++ scripts/impersonate.ts
@@ -1,27 +1,34 @@
 import {ethers, upgrades} from "hardhat";
 import {
+  CLANS_ADDRESS,
   ITEM_NFT_ADDRESS,
+  LOCKED_BANK_VAULT_ADDRESS,
   PLAYERS_ADDRESS,
   PLAYER_NFT_ADDRESS,
   PROMOTIONS_ADDRESS,
   QUESTS_ADDRESS,
   SHOP_ADDRESS,
+  TERRITORIES_ADDRESS,
   WORLD_ADDRESS,
 } from "./contractAddresses";
 import {deployPlayerImplementations} from "./utils";
+import {Clans} from "../typechain-types";
+import {ClanRank} from "@paintswap/estfor-definitions/types";
 
 // When you need to fork a chain and debug
 async function main() {
   const network = await ethers.provider.getNetwork();
   console.log(`ChainId: ${network.chainId}`);
 
+  const owner = await ethers.getImpersonatedSigner("0x316342122A9ae36de41B231260579b92F4C8Be7f");
+  const player = await ethers.getImpersonatedSigner("0xe13894604b6dc9523a9822dfa2909c2a9cd084a6");
+  const playerId = 892;
+  const EstforLibrary = await ethers.getContractFactory("EstforLibrary");
+  let estforLibrary = await EstforLibrary.deploy();
+
   // Players
   const PlayersLibrary = await ethers.getContractFactory("PlayersLibrary");
   const playersLibrary = await PlayersLibrary.deploy();
-
-  const owner = await ethers.getImpersonatedSigner("0x316342122A9ae36de41B231260579b92F4C8Be7f");
-  const player = await ethers.getImpersonatedSigner("0x6dc225f7f21acb842761b8df52ae46208705c942");
-  const playerId = 158;
 
   // Set the implementations
   let Players = await ethers.getContractFactory("Players");
@@ -46,8 +53,6 @@ async function main() {
   await tx.wait();
 
   // PlayerNFT
-  const EstforLibrary = await ethers.getContractFactory("EstforLibrary");
-  let estforLibrary = await EstforLibrary.deploy();
   await estforLibrary.deployed();
   let PlayerNFT = await ethers.getContractFactory("PlayerNFT", {
     libraries: {EstforLibrary: estforLibrary.address},
@@ -101,8 +106,30 @@ async function main() {
     kind: "uups",
   });
 
-  const pendingQueuedActionState = await players.pendingQueuedActionState(player.address, playerId);
-  console.log(pendingQueuedActionState);
+  let Clans = (
+    await ethers.getContractFactory("Clans", {
+      libraries: {EstforLibrary: estforLibrary.address},
+    })
+  ).connect(owner);
+  const clans = (await upgrades.upgradeProxy(CLANS_ADDRESS, Clans, {
+    kind: "uups",
+    unsafeAllow: ["external-library-linking"],
+  })) as Clans;
+
+  const LockedBankVaults = (await ethers.getContractFactory("LockedBankVaults")).connect(owner);
+  const lockedBankVaults = await upgrades.upgradeProxy(LOCKED_BANK_VAULT_ADDRESS, LockedBankVaults, {
+    kind: "uups",
+    unsafeAllow: ["external-library-linking"],
+  });
+
+  const Territories = (await ethers.getContractFactory("Territories")).connect(owner);
+  const territories = await upgrades.upgradeProxy(TERRITORIES_ADDRESS, Territories, {
+    kind: "uups",
+    unsafeAllow: ["external-library-linking"],
+  });
+
+  //  const pendingQueuedActionState = await players.pendingQueuedActionState(player.address, playerId);
+  //  console.log(pendingQueuedActionState);
 }
 
 main().catch((error) => {
