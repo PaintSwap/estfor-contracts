c813aa6f97cd9ddfbe6259145bbb2c02f2d15445
0xSamWitch <julian.assange12@outlook.com>
2024-04-20 10:25:03 +0100

    Add setMinItemQuantityBeforeSellsAllowed script

---
+++ scripts/setMinItemQuantityBeforeSellsAllowed.ts
@@ -0,0 +1,17 @@
+import {ethers} from "hardhat";
+import {SHOP_ADDRESS} from "./contractAddresses";
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  console.log(
+    `Set minimum quantity before sells are allowed: ${owner.address} on chain id ${await owner.getChainId()}`
+  );
+
+  const shop = await ethers.getContractAt("Shop", SHOP_ADDRESS);
+  await shop.setMinItemQuantityBeforeSellsAllowed(500);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
