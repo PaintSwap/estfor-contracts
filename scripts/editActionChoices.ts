0b686330345ef6051065e0ce614dda13ad793f99
0xSamWitch <julian.assange12@outlook.com>
2024-05-06 19:34:06 +0100

    Update forging infused orichalcum xp/hr

--- scripts/editActionChoices.ts
+++ scripts/editActionChoices.ts
@@ -10,22 +10,35 @@ async function main() {
 
   const world = await ethers.getContractAt("World", WORLD_ADDRESS);
 
-  // Single
-  /*  const index = await allActionChoiceIdsFletching.findIndex(
-    (actionChoiceId) => actionChoiceId === EstforConstants.ACTIONCHOICE_FLETCHING_BASIC_BOW
-  );
-  if (index === -1) {
-    console.error("ActionChoiceId not found");
+  const actionChoicesToUpdate = new Set([
+    EstforConstants.ACTIONCHOICE_FORGING_ORICHALCUM_ARMOR,
+    EstforConstants.ACTIONCHOICE_FORGING_ORICHALCUM_HELMET,
+    EstforConstants.ACTIONCHOICE_FORGING_ORICHALCUM_TASSETS,
+    EstforConstants.ACTIONCHOICE_FORGING_ORICHALCUM_GAUNTLETS,
+    EstforConstants.ACTIONCHOICE_FORGING_ORICHALCUM_BOOTS,
+    EstforConstants.ACTIONCHOICE_FORGING_ORICHALCUM_SHIELD,
+  ]);
+
+  const actionChoiceIndices = allActionChoiceIdsForging.reduce((indices: number[], actionChoiceId, index) => {
+    if (actionChoicesToUpdate.has(actionChoiceId)) {
+      indices.push(index);
+    }
+    return indices;
+  }, []);
+
+  const actionChoices = actionChoiceIndices.map((index) => allActionChoicesForging[index]);
+  const actionChoiceIds = actionChoiceIndices.map((index) => allActionChoiceIdsForging[index]);
+
+  if (actionChoices.length !== actionChoicesToUpdate.size || actionChoiceIds.length !== actionChoicesToUpdate.size) {
+    console.error("ActionChoiceIds not found");
   } else {
     {
-      const tx = await world.editActionChoices(
-        EstforConstants.ACTION_FLETCHING_ITEM,
-        [allActionChoiceIdsFletching[index]],
-        [allActionChoicesFletching[index]]
-      );
+      const tx = await world.editActionChoices(EstforConstants.ACTION_FORGING_ITEM, actionChoiceIds, actionChoices);
       await tx.wait();
     }
-  } */
+  }
+
+  /*
   {
     const tx = await world.editActionChoices(
       EstforConstants.ACTION_FORGING_ITEM,
@@ -33,7 +46,7 @@ async function main() {
       allActionChoicesForging
     );
     await tx.wait();
-  }
+  } */
 }
 
 main().catch((error) => {
