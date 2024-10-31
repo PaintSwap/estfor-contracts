111ae8f086dc7501f3e193c9741ef44044b2071c
0xSamWitch <julian.assange12@outlook.com>
2024-10-31 22:31:55 +0000

    Add unclaimed emissions script

---
+++ scripts/addUnclaimedEmissions.ts
@@ -0,0 +1,22 @@
+import {ethers} from "hardhat";
+import {BRUSH_ADDRESS, TERRITORIES_ADDRESS} from "./contractAddresses";
+import {getChainId} from "./utils";
+import {MockBrushToken, Territories} from "../typechain-types";
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  console.log(`Sent some brush to the territories: ${owner.address} on chain id ${await getChainId(owner)}`);
+  const territories = (await ethers.getContractAt("Territories", TERRITORIES_ADDRESS)).connect(owner) as Territories;
+
+  const brush = (await ethers.getContractAt("MockBrushToken", BRUSH_ADDRESS)) as MockBrushToken;
+  let tx = await brush.approve(TERRITORIES_ADDRESS, ethers.parseEther("100000"));
+  await tx.wait();
+
+  tx = await territories.addUnclaimedEmissions(ethers.parseEther("100"));
+  await tx.wait();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
