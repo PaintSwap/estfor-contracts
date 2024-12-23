d8f282114e212ee80acbef1ade990fbee446d09a
0xSamWitch <julian.assange12@outlook.com>
2024-12-23 18:44:15 +0000

    Update samwitchVRF

--- scripts/contractAddresses.ts
+++ scripts/contractAddresses.ts
@@ -59,7 +59,7 @@ if (chainId == "146") {
   wftm = "0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38";
   if (!isBeta) {
     oracle = "0x28ade840602d0363a2ab675479f1b590b23b0490";
-    samWitchVRF = "0xdA3EcEfD34C7ECF4f39C65173F78de498FF3060a";
+    samWitchVRF = "0xE8eE3F1F75Df6807C78adDd1eB3edd010aBEe127";
   } else {
     oracle = "0x6f7911cbbd4b5a1d2bdaa817a76056e510d728e7";
     samWitchVRF = "0x1BFf1DC67974577CF62A76463580CA5BBcC5f68e";
