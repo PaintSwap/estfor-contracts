bf92673f881132680f132619f7a394250eb3df13
0xSamWitch <julian.assange12@outlook.com>
2024-12-24 20:36:36 +0000

    Add mainnet bridge address to the setPeer script

--- scripts/setPeer.ts
+++ scripts/setPeer.ts
@@ -6,7 +6,9 @@ import {BRIDGE_ADDRESS} from "./contractAddresses";
 async function main() {
   const bridge = await ethers.getContractAt("Bridge", BRIDGE_ADDRESS);
   const dstEid = 30112; // Fantom
-  let dstBridgeAddress = isBeta ? "0x93e478f428a070ecf11d79cdd6a60f3dee3a92da" : "";
+  let dstBridgeAddress = isBeta
+    ? "0x93e478f428a070ecf11d79cdd6a60f3dee3a92da"
+    : "0x4381ba70358b46e220b3e9188acfef224e9f8a8f";
 
   const tx = await bridge.setPeer(dstEid, ethers.zeroPadValue(dstBridgeAddress, 32));
   await tx.wait();
