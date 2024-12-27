418bf938bc1d48accaf8490cd01dcd9ba94cda9f
0xSamWitch <julian.assange12@outlook.com>
2024-12-27 12:21:37 +0000

    Update book spell magic diffs

--- scripts/editActionChoices.ts
+++ scripts/editActionChoices.ts
@@ -1,7 +1,7 @@
 import {ethers} from "hardhat";
 import {WORLD_ACTIONS_ADDRESS} from "./contractAddresses";
-import {allActionChoicesRanged} from "./data/actionChoices";
-import {allActionChoiceIdsRanged} from "./data/actionChoiceIds";
+import {allActionChoicesMagic} from "./data/actionChoices";
+import {allActionChoiceIdsMagic} from "./data/actionChoiceIds";
 import {EstforConstants} from "@paintswap/estfor-definitions";
 import {getChainId} from "./utils";
 
@@ -12,19 +12,32 @@ async function main() {
   const worldActions = await ethers.getContractAt("WorldActions", WORLD_ACTIONS_ADDRESS);
 
   const actionChoicesToUpdate = new Set([
-    EstforConstants.ACTIONCHOICE_RANGED_BASIC_BOW_FIRE,
-    EstforConstants.ACTIONCHOICE_RANGED_BASIC_BOW_POISON
+    EstforConstants.ACTIONCHOICE_MAGIC_SHADOW_BOLT,
+    EstforConstants.ACTIONCHOICE_MAGIC_RAZOR_LEAVES,
+    EstforConstants.ACTIONCHOICE_MAGIC_HEX,
+    EstforConstants.ACTIONCHOICE_MAGIC_TORRENT_SURGE,
+    EstforConstants.ACTIONCHOICE_MAGIC_HYDRO_BURST,
+    EstforConstants.ACTIONCHOICE_MAGIC_VOID_WAVE,
+    EstforConstants.ACTIONCHOICE_MAGIC_RAISE_DEAD,
+    EstforConstants.ACTIONCHOICE_MAGIC_TORNADO,
+    EstforConstants.ACTIONCHOICE_MAGIC_SUMMON_WIND_FAMILIAR,
+    EstforConstants.ACTIONCHOICE_MAGIC_ARCANE_NOVA,
+    EstforConstants.ACTIONCHOICE_MAGIC_FROST_BREATH,
+    EstforConstants.ACTIONCHOICE_MAGIC_LIFE_DRAIN,
+    EstforConstants.ACTIONCHOICE_MAGIC_BLIZZARD,
+    EstforConstants.ACTIONCHOICE_MAGIC_STARFALL,
+    EstforConstants.ACTIONCHOICE_MAGIC_CHRONO_FREEZE
   ]);
 
-  const actionChoiceIndices = allActionChoiceIdsRanged.reduce((indices: number[], actionChoiceId, index) => {
+  const actionChoiceIndices = allActionChoiceIdsMagic.reduce((indices: number[], actionChoiceId, index) => {
     if (actionChoicesToUpdate.has(actionChoiceId)) {
       indices.push(index);
     }
     return indices;
   }, []);
 
-  const actionChoices = actionChoiceIndices.map((index) => allActionChoicesRanged[index]);
-  const actionChoiceIds = actionChoiceIndices.map((index) => allActionChoiceIdsRanged[index]);
+  const actionChoices = actionChoiceIndices.map((index) => allActionChoicesMagic[index]);
+  const actionChoiceIds = actionChoiceIndices.map((index) => allActionChoiceIdsMagic[index]);
 
   if (actionChoices.length !== actionChoicesToUpdate.size || actionChoiceIds.length !== actionChoicesToUpdate.size) {
     console.error("ActionChoiceIds not found");
