a7826b7e787fa71917824b266c352d0b1f825434
0xSamWitch <julian.assange12@outlook.com>
2025-02-03 14:14:27 +0000

    Fix quest action for QUEST_GROWING_CONCERNS_V

--- scripts/editQuests.ts
+++ scripts/editQuests.ts
@@ -9,7 +9,7 @@ async function main() {
   console.log(`Edit quest using account: ${owner.address} on chain id ${await getChainId(owner)}`);
 
   const quests = await ethers.getContractAt("Quests", QUESTS_ADDRESS);
-  const questsRaw = new Set([EstforConstants.QUEST_APPRENTICESHIP_II, EstforConstants.QUEST_IRON_AGE_II]);
+  const questsRaw = new Set([EstforConstants.QUEST_GROWING_CONCERNS_V]);
 
   const questIndexes = allQuests.map((q, index) => (questsRaw.has(q.questId) ? index : "")).filter(String) as number[];
   if (questIndexes.length != questsRaw.size) {
