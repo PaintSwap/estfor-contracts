05b77e56ff7ef179f9e4b89da859de5c7473d153
deif <deif@pm.me>
2025-12-28 14:51:51 +0000

    update boost items to non-transferable

--- scripts/editItems.ts
+++ scripts/editItems.ts
@@ -3,13 +3,30 @@ import {allItems} from "./data/items";
 import {ITEM_NFT_ADDRESS} from "./contractAddresses";
 import {EstforConstants} from "@paintswap/estfor-definitions";
 import {getChainId} from "./utils";
+import {initialiseSafe, sendTransactionSetToSafe} from "./utils";
+import {OperationType, MetaTransactionData} from "@safe-global/types-kit";
 
 async function main() {
-  const [owner] = await ethers.getSigners();
+  const [owner, , proposer] = await ethers.getSigners();
+  const network = await ethers.provider.getNetwork();
+  const {useSafe, apiKit, protocolKit} = await initialiseSafe(network);
   console.log(`Edit items using account: ${owner.address} on chain id ${await getChainId(owner)}`);
 
   const itemNFT = await ethers.getContractAt("ItemNFT", ITEM_NFT_ADDRESS);
-  const itemsToEdit = new Set([EstforConstants.SONIC_GEM_TICKET, EstforConstants.ACTIVITY_TICKET]);
+  const itemsToEdit = new Set([
+    EstforConstants.XP_BOOST_M,
+    EstforConstants.COMBAT_BOOST_M,
+    EstforConstants.SKILL_BOOST_M,
+    EstforConstants.GATHERING_BOOST_M,
+    EstforConstants.XP_BOOST_L,
+    EstforConstants.COMBAT_BOOST_L,
+    EstforConstants.SKILL_BOOST_L,
+    EstforConstants.GATHERING_BOOST_L,
+    EstforConstants.XP_BOOST_XL,
+    EstforConstants.COMBAT_BOOST_XL,
+    EstforConstants.SKILL_BOOST_XL,
+    EstforConstants.GATHERING_BOOST_XL
+  ]);
 
   const items = allItems.filter((item) => itemsToEdit.has(item.tokenId));
   /*
@@ -26,7 +43,20 @@ async function main() {
   if (items.length !== itemsToEdit.size) {
     console.log("Cannot find all items");
   } else {
-    await itemNFT.editItems(items);
+    if (useSafe) {
+      const transactionSet: MetaTransactionData[] = [];
+      const contract = await ethers.getContractAt("ItemNFT", ITEM_NFT_ADDRESS);
+      const iface = contract.interface;
+      transactionSet.push({
+        to: ethers.getAddress(ITEM_NFT_ADDRESS),
+        value: "0",
+        data: iface.encodeFunctionData("editItems", [items]),
+        operation: OperationType.Call
+      });
+      await sendTransactionSetToSafe(network, protocolKit, apiKit, transactionSet, proposer);
+    } else {
+      await itemNFT.editItems(items);
+    }
   }
 }
 
