1ac10bbf5e86f1248f376f6b5ea2bfebf8548666
deif <deif@pm.me>
2026-01-23 17:17:17 +0000

    update base uri script to use safe

--- scripts/setItemsBaseURI.ts
+++ scripts/setItemsBaseURI.ts
@@ -1,13 +1,33 @@
 import {ethers} from "hardhat";
 import {ITEM_NFT_ADDRESS} from "./contractAddresses";
-import {getChainId} from "./utils";
+
+import {getSafeUpgradeTransaction, initialiseSafe, sendTransactionSetToSafe, isBeta} from "./utils";
+import {OperationType, MetaTransactionData} from "@safe-global/types-kit";
+import {ItemNFT__factory} from "../typechain-types";
 
 async function main() {
-  const [owner] = await ethers.getSigners();
-  console.log(`Set itemNFT base uri with: ${owner.address} on chain id ${await getChainId(owner)}`);
+  const [owner, , proposer] = await ethers.getSigners(); // 0 is old deployer, 2 is proposer for Safe (new deployer)
+  const network = await ethers.provider.getNetwork();
+  const {useSafe, apiKit, protocolKit} = await initialiseSafe(network);
+  console.log(`Set itemNFT base uri with: ${owner.address} on chain id ${network.chainId}, useSafe: ${useSafe}`);
 
-  const itemNFT = await ethers.getContractAt("ItemNFT", ITEM_NFT_ADDRESS);
-  await itemNFT.setBaseURI("ipfs://bafybeic53fzh4owvyz3uiex32es3bawcbyxfkn3lmj3prlpj4xup2sxkfm/");
+  if (useSafe) {
+    const transactionSet: MetaTransactionData[] = [];
+    const iface = ItemNFT__factory.createInterface();
+
+    transactionSet.push({
+      to: ethers.getAddress(ITEM_NFT_ADDRESS),
+      value: "0",
+      data: iface.encodeFunctionData("setBaseURI", [
+        "ipfs://bafybeiggbntyqyxzppi5u7c4e4kwxyzvfo2vqemlocbwyjmgnpztuklh4y/",
+      ]),
+      operation: OperationType.Call,
+    });
+    await sendTransactionSetToSafe(network, protocolKit, apiKit, transactionSet, proposer);
+  } else {
+    const itemNFT = await ethers.getContractAt("ItemNFT", ITEM_NFT_ADDRESS);
+    await itemNFT.setBaseURI("ipfs://bafybeiggbntyqyxzppi5u7c4e4kwxyzvfo2vqemlocbwyjmgnpztuklh4y/");
+  }
 }
 
 main().catch((error) => {
